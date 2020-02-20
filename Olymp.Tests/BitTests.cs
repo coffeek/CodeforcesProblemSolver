88457407278258e34bbfaaaf20e1e3f0a5de44a0
coffeek <ya.kofeek@yandex.ru>
2020-02-20 22:06:29 +0400

    BIT

---
+++ Olymp.Tests/BitTests.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using NUnit.Framework;
+
+namespace Olymp.Tests
+{
+  [TestFixture]
+  public class BitTests
+  {
+    [Test]
+    public void SumTest()
+    {
+      var a = new[] { 21, -14, 19, -41, 3, 35, -26, 35, 23, -36, -27, 6, 37, -23, 45, -8, -15, -29, 21, 0 };
+      var t = new Bit(a);
+      for (int i = 0; i < a.Length; i++)
+      {
+        Assert.That(t.Sum(i), Is.EqualTo(a[..(i + 1)].Sum()));
+        for (int j = i; j < a.Length; j++)
+          Assert.That(t.Sum(i, j), Is.EqualTo(a[i..(j + 1)].Sum()));
+      }
+    }
+
+    [Test]
+    public void SumAfterChangeTest()
+    {
+      var a = new[] { -1, -2, 0, 5, 5, 3 };
+      
+      var t = new Bit(a);
+      
+      Assert.That(t.Sum(0, 1), Is.EqualTo(-3));
+      t.Add(1, 2);
+      Assert.That(t.Sum(0, 1), Is.EqualTo(-1));
+      
+      Assert.That(t.Sum(2, 5), Is.EqualTo(13));
+      t.Add(3, 1);
+      t.Add(4, -2);
+      Assert.That(t.Sum(2, 5), Is.EqualTo(12));
+    }
+  }
+}
---
+++ olymp/Bit.cs
@@ -0,0 +1,39 @@
+namespace Olymp
+{
+  public class Bit
+  {
+    private readonly int n;
+    private readonly int[] bit;
+
+    public int Sum(int r)
+    {
+      int ret = 0;
+      for (; r >= 0; r = (r & (r + 1)) - 1)
+        ret += bit[r];
+      return ret;
+    }
+
+    public int Sum(int l, int r)
+    {
+      return Sum(r) - Sum(l - 1);
+    }
+
+    public void Add(int index, int value)
+    {
+      for (; index < n; index |= (index + 1))
+        bit[index] += value;
+    }
+
+    public Bit(int n)
+    {
+      this.n = n;
+      bit = new int[n];
+    }
+
+    public Bit(int[] a) : this(a.Length)
+    {
+      for (int i = 0; i < a.Length; i++)
+        Add(i, a[i]);
+    }
+  }
+}
