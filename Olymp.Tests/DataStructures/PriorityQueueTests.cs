0d936b8ee82f5ffbea210a2f05e34bd5cfa71234
coffeek <ya.kofeek@yandex.ru>
2021-11-13 01:08:32 +0400

    Added fast priority queue

---
+++ Olymp.Tests/DataStructures/PriorityQueueTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Olymp.DataStructures;
+
+namespace Olymp.Tests.DataStructures
+{
+  [TestFixture]
+  public class PriorityQueueTests
+  {
+    [Test]
+    public void EnqueueTest()
+    {
+      var q = new PriorityQueue<int>();
+      q.Enqueue(10);
+      q.Top.Should().Be(10);
+      q.Enqueue(9);
+      q.Top.Should().Be(9);
+      q.Enqueue(10);
+      q.Top.Should().Be(9);
+      q.Enqueue(5);
+      q.Top.Should().Be(5);
+      q.Enqueue(-1);
+      q.Top.Should().Be(-1);
+      
+      q.Count.Should().Be(5);
+    }
+    
+    [Test]
+    public void DequeueTest()
+    {
+      var q = new PriorityQueue<int>();
+      var arr = new[] { 10, -1, 20, 7, 10, 0, 1, 0 };
+      foreach (var num in arr)
+        q.Enqueue(num);
+
+      q.Count.Should().Be(arr.Length);
+      
+      q.Top.Should().Be(-1);
+      
+      q.Dequeue().Should().Be(-1);
+      q.Top.Should().Be(0);
+      
+      q.Dequeue().Should().Be(0);
+      q.Top.Should().Be(0);
+      
+      q.Dequeue().Should().Be(0);
+      q.Top.Should().Be(1);
+      
+      q.Dequeue().Should().Be(1);
+      q.Top.Should().Be(7);
+      
+      q.Dequeue().Should().Be(7);
+      q.Top.Should().Be(10);
+      
+      q.Count.Should().Be(arr.Length - 5);
+    }
+  }
+}
---
+++ olymp/DataStructures/PriorityQueue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Olymp.DataStructures
+{
+  public class PriorityQueue<T>
+  {
+    private readonly Comparison<T> compare;
+    private readonly List<T> heap;
+    
+    public int Count => heap.Count;
+    
+    public T Top
+    {
+      get
+      {
+        if (heap.Count == 0)
+          throw new InvalidOperationException("Queue is empty");
+        return heap[0];
+      }
+    }
+    
+    public void Enqueue(T item)
+    {
+      var i = heap.Count;
+      heap.Add(item);
+      while (i > 0)
+      {
+        var p = (i - 1) >> 1;
+        if (compare(heap[p], item) <= 0)
+          break;
+        heap[i] = heap[p];
+        i = p;
+      }
+      heap[i] = item;
+    }
+
+    public T Dequeue()
+    {
+      if (heap.Count == 0)
+        throw new InvalidOperationException("Queue is empty");
+      
+      var result = heap[0];
+      var last = heap.Count - 1;
+      var lastItem = heap[^1];
+      var i = 0;
+      while (((i << 1) | 1) < last)
+      {
+        var lch = (i << 1) | 1;
+        var rch = lch + 1;
+        if (rch < last && compare(heap[rch], heap[lch]) < 0)
+          lch = rch;
+        if (compare(lastItem, heap[lch]) < 0)
+          break;
+        heap[i] = heap[lch];
+        i = lch;
+      }
+      heap[i] = lastItem;
+      heap.RemoveAt(last);
+      return result;
+    }
+
+    public PriorityQueue(Comparison<T> comparison = null)
+    {
+      compare = comparison ?? Comparer<T>.Default.Compare;
+      heap = new List<T>();
+    }
+  }
+}
