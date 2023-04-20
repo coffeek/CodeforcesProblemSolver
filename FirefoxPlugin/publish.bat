945909f7b5feea902fdb255dc81f959c05cdfff4
coffeek <ya.kofeek@yandex.ru>
2023-04-20 23:47:45 +0400

    publish.bat fix

--- FirefoxPlugin/publish.bat
+++ FirefoxPlugin/publish.bat
@@ -1,4 +1,4 @@
-# https://extensionworkshop.com/documentation/develop/web-ext-command-reference/
-# Значения для ключей --api-key и --api-secret берутся из переменных окружения $WEB_EXT_API_KEY и $WEB_EXT_API_SECRET.
-# Для подписания нужно создать эти переменные и указать значения из https://addons.mozilla.org/en-US/developers/addon/api/key/
+rem https://extensionworkshop.com/documentation/develop/web-ext-command-reference/
+rem Значения для ключей --api-key и --api-secret берутся из переменных окружения $WEB_EXT_API_KEY и $WEB_EXT_API_SECRET.
+rem Для подписания нужно создать эти переменные и указать значения из https://addons.mozilla.org/en-US/developers/addon/api/key/
 web-ext sign --channel unlisted --artifacts-dir ./artifacts --source-dir ./src --overwrite-dest
