16d84f27e614e5d790bbf2e1906725d33e22c02b
coffeek <ya.kofeek@yandex.ru>
2023-04-21 23:50:32 +0400

    Simple Depth-First Search

---
+++ Solver/Graphs/Dfs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solver.Graphs;
+
+/// <summary>
+/// Depth-First Search.
+/// </summary>
+public class Dfs
+{
+  private readonly List<List<int>> g;
+  private readonly Action<int> action;
+  private readonly bool[] visited;
+  
+  public Dfs(List<List<int>> g, Action<int> action)
+  {
+    this.g = g;
+    this.action = action;
+    visited = new bool[g.Count];
+  }
+
+  public void Run(int from)
+  {
+    action(from);
+    visited[from] = true;
+    if (g[from] is null)
+      return;
+    for (int i = 0; i < g[from].Count; i++)
+    {
+      if (!visited[from])
+        Run(from);
+    }
+  }
+}
