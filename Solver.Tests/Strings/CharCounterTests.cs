c994fbfe86cb19e95b2e8cef298a929e98e9ef7a
coffeek <ya.kofeek@yandex.ru>
2023-04-23 13:57:15 +0400

    CharCounter

---
+++ Solver.Tests/Strings/CharCounterTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Solver.Strings;
+
+namespace Solver.Tests.Strings;
+
+[TestFixture]
+public class CharCounterTests
+{
+  [Test]
+  public void BasicUsage()
+  {
+    var counter = new CharCounter();
+    counter.DistinctCount.Should().Be(0);
+    counter.GetCount('a').Should().Be(0);
+    counter.Add("asdf");
+    counter.DistinctCount.Should().Be(4);
+    counter.GetCount('a').Should().Be(1);
+    counter.Add("abzaa");
+    counter.DistinctCount.Should().Be(6);
+    counter.GetCount('a').Should().Be(4);
+    counter.Remove('a');
+    counter.GetCount('a').Should().Be(3);
+    counter.DistinctCount.Should().Be(6);
+    counter.Remove('a');
+    counter.GetCount('a').Should().Be(2);
+    counter.Remove('z');
+    counter.GetCount('z').Should().Be(0);
+    counter.DistinctCount.Should().Be(5);
+    counter.Remove('z');
+    counter.GetCount('z').Should().Be(0);
+    counter.DistinctCount.Should().Be(5);
+  }
+}
---
+++ Solver/Strings/CharCounter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Solver.Strings;
+
+public class CharCounter
+{
+  private readonly int[] count = new int['z' - 'a' + 1];
+  private int distinct;
+
+  public int DistinctCount => distinct;
+  public int GetCount(char c) => count[c - 'a'];
+
+  public void Add(char c)
+  {
+    if (count[c - 'a']++ == 0)
+      distinct++;
+  }
+
+  public void Add(ReadOnlySpan<char> s)
+  {
+    for (int i = 0; i < s.Length; i++)
+      Add(s[i]);
+  }
+
+  public void Remove(char c)
+  {
+    var i = c - 'a';
+    if (count[i] == 0)
+      return;
+    if (--count[i] == 0)
+      distinct--;
+  }
+}
