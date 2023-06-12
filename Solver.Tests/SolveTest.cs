306dd4153e5e5815106ea4fbe53b81835cab6790
coffeek <ya.kofeek@yandex.ru>
2023-06-12 22:04:09 +0400

    Commented the template for the tests

--- Solver.Tests/SolveTest.cs
+++ Solver.Tests/SolveTest.cs
@@ -6,41 +6,14 @@ namespace Solver.Tests;
 [TestFixture]
 public class SolveTest
 {
-  [Test]
-  public void Case1()
-  {
-    Assert.AreEqual(
-      @"",
-      GetResult(
-        @""));
-  }
-
-  [Test]
-  public void Case2()
-  {
-    Assert.AreEqual(
-      @"",
-      GetResult(
-        @""));
-  }
-
-  [Test]
-  public void Case3()
-  {
-    Assert.AreEqual(
-      @"",
-      GetResult(
-        @""));
-  }
-
-  [Test]
-  public void Case4()
-  {
-    Assert.AreEqual(
-      @"",
-      GetResult(
-        @""));
-  }
+  // [Test]
+  // public void Case1()
+  // {
+  //   Assert.AreEqual(
+  //     @"",
+  //     GetResult(
+  //       @""));
+  // }
 
   private static string GetResult(string inputData)
   {
