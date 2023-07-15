71154432a005d99df935c5f259d1b644550d4246
coffeek <ya.kofeek@yandex.ru>
2023-07-15 17:33:25 +0400

    UniqueCharCounter tests

---
+++ Solver.Tests/DataStructures/SegmentTree/UniqueCharCounterTests.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using Solver.DataStructures.SegmentTree;
+
+namespace Solver.Tests.DataStructures.SegmentTree;
+
+[TestFixture]
+public class UniqueCharCounterTests
+{
+  [Test]
+  public void QueryTest()
+  {
+    const string s = "abaabcaaooiodmzmammabbxdaasjwo";
+    var counter = new UniqueCharCounter(s);
+    CheckQueriesCorrectness(counter, s);
+  }
+
+  [Test]
+  public void TestUpdate()
+  {
+    const string s = "abaabcaaooiodmzmammabbxdaasjwo";
+    var counter = new UniqueCharCounter(s);
+    var sb = new StringBuilder(s);
+    for (int i = 0; i < s.Length; i++)
+    {
+      sb[i] += (char)1;
+      counter.Update(i, sb[i]);
+      CheckQueriesCorrectness(counter, sb.ToString());
+    }
+  }
+
+  private static void CheckQueriesCorrectness(UniqueCharCounter counter, string s)
+  {
+    for (int i = 0; i < s.Length; i++)
+    for (int j = i; j < s.Length; j++)
+    {
+      counter.GetCount(i, j).Should().Be(s[i..(j + 1)].Distinct().Count(), "Query({0}, {1})", i, j);
+    }
+  }
+}
