8af80ebad70d889ccc0f3d2d3e63b340b2892fe3
coffeek <ya.kofeek@yandex.ru>
2023-07-21 21:21:26 +0400

    Added algorithm to find the number of LIS

--- Solver/Sequences/LisAlgorithms.cs
+++ Solver/Sequences/LisAlgorithms.cs
@@ -23,4 +23,42 @@ public static class LisAlgorithms
     }
     return length.Max();
   }
+
+
+
+  /// <summary>
+  /// Return the number of longest increasing subsequences.
+  /// </summary>
+  public static int FindNumberOfLis(int[] numbers)
+  {
+    var n = numbers.Length;
+    var length = new int[n];
+    var count = new int[n];
+    Array.Fill(length, 1);
+    Array.Fill(count, 1);
+    for (var i = 1; i < n; i++)
+    {
+      for (var j = 0; j < i; j++)
+      {
+        if (numbers[j] < numbers[i])
+        {
+          if (length[j] + 1 > length[i])
+          {
+            length[i] = Math.Max(length[i], length[j] + 1);
+            count[i] = 0;
+          }
+          if (length[j] + 1 == length[i])
+            count[i] += count[j];
+        }
+      }
+    }
+    var maxLength = length.Max();
+    var numberOfLis = 0;
+    for (var i = 0; i < n; i++)
+    {
+      if (length[i] == maxLength)
+        numberOfLis += count[i];
+    }
+    return numberOfLis;
+  }
 }
