ff37818aa893efdbb0a1ace9d42c00db520e9a45
coffeek <ya.kofeek@yandex.ru>
2023-11-01 10:59:30 +0400

    Boyer–Moore majority vote algorithm (adaptation for n/3 majority)

--- Solver.Tests/Sequences/MajorityTests.cs
+++ Solver.Tests/Sequences/MajorityTests.cs
@@ -9,8 +9,17 @@ public class MajorityTests
 {
   [TestCase(new[] { 3, 2, 3 }, 3)]
   [TestCase(new[] { 2, 2, 1, 1, 1, 2, 2 }, 2)]
-  public void Test(int[] a, int expected)
+  public void MajorityElement(int[] a, int expected)
   {
     new Majority().MajorityElement(a).Should().Be(expected);
+  }
+  
+  [TestCase(new[] { 3, 2, 3 }, new[] { 3 })]
+  [TestCase(new[] { 1 }, new[] { 1 })]
+  [TestCase(new[] { 1, 2 }, new[] { 1, 2 })]
+  [TestCase(new[] { 1, 3, 4, 3, 3, 3, 1, 3, 5, 6, 1, 5, 3, 1, 5, 1, 1 }, new[] { 1, 3 })]
+  public void ThirdMajorityElements(int[] a, int[] expected)
+  {
+    new Majority().ThirdMajorityElements(a).Should().BeEquivalentTo(expected);
   }
 }
--- Solver/Sequences/Majority.cs
+++ Solver/Sequences/Majority.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Solver.Sequences;
 
 public class Majority
@@ -32,5 +34,51 @@ public class Majority
       }
     }
     return major;
+  }
+  
+  /// <summary>
+  /// Find all elements that appear more than ⌊ n/3 ⌋ times.
+  /// </summary>
+  /// <remarks>
+  /// Boyer–Moore majority vote algorithm (adaptation for n/3 majority)
+  /// https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+  /// </remarks>
+  public IList<int> ThirdMajorityElements(int[] nums)
+  {
+    var count1 = 0;
+    var count2 = 0;
+    var major1 = 0;
+    var major2 = 0;
+    foreach (var n in nums)
+    {
+      if (n == major1)
+        count1++;
+      else if (n == major2)
+        count2++;
+      else if (count1 == 0)
+        (major1, count1) = (n, 1);
+      else if (count2 == 0)
+        (major2, count2) = (n, 1);
+      else
+      {
+        count1--;
+        count2--;
+      }
+    }
+    count1 = 0;
+    count2 = 0;
+    foreach (var n in nums)
+    {
+      if (n == major1)
+        count1++;
+      else if (n == major2)
+        count2++;
+    }
+    var ans = new List<int>();
+    if (count1 > nums.Length / 3)
+      ans.Add(major1);
+    if (count2 > nums.Length / 3)
+      ans.Add(major2);
+    return ans;
   }
 }
