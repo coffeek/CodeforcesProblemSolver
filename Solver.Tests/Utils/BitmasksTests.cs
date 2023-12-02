fb0ec7a1958619a56dcb94f684adfc09271ee265
coffeek <ya.kofeek@yandex.ru>
2023-12-02 19:35:22 +0400

    Added bitmasks utils & tests

---
+++ Solver.Tests/Utils/BitmasksTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Solver.Utils;
+
+namespace Solver.Tests.Utils;
+
+[TestFixture]
+public class BitmasksTests
+{
+  [TestCase(0u, 0u)]
+  [TestCase(1u, 1u << 31)]
+  [TestCase(1u << 31, 1u)]
+  [TestCase(0b00110110110010110110000110101110u, 0b01110101100001101101001101101100u)]
+  public void ReverseBits(uint n, uint expected)
+  {
+    Bitmasks.ReverseBits(n).Should().Be(expected);
+    Bitmasks.ReverseBits2(n).Should().Be(expected);
+  }
+
+  [TestCase(0u, 0u)]
+  [TestCase(0b1u, 0b1u)]
+  [TestCase(0b11u, 0b10u)]
+  [TestCase(0b10101010100101u, 0b10000000000000u)]
+  [TestCase(uint.MaxValue, 1u << 31)]
+  [TestCase((1u << 31) + 1, 1u << 31)]
+  public void LargestPower(uint n, uint expected)
+  {
+    Bitmasks.LargestPower(n).Should().Be(expected);
+    Bitmasks.LargestPower2(n).Should().Be(expected);
+  }
+}
---
+++ Solver/Utils/Bitmasks.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Numerics;
+
+namespace Solver.Utils;
+
+public static class Bitmasks
+{
+  public static uint ReverseBits(uint n)
+  {
+    uint result = 0;
+    for (var i = 0; i < 32; i++)
+    {
+      result = (result << 1) | (n & 1);
+      n >>= 1;
+    }
+    return result;
+  }
+
+  public static uint ReverseBits2(UInt32 n)
+  {
+    n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+    n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+    n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+    n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+    n = ((n & 0xffff0000) >> 16) | ((n & 0x0000ffff) << 16);
+    return n;
+  }
+
+  public static uint LargestPower(uint n)
+  {
+    const uint maxPow = 1u << 31;
+    if ((n & maxPow) != 0)
+      return maxPow;
+    // Fill trailing zeros with ones, eg 00010010 becomes 00011111.
+    n |= (n >> 1);
+    n |= (n >> 2);
+    n |= (n >> 4);
+    n |= (n >> 8);
+    n |= (n >> 16);
+    return (n + 1) >> 1;
+  }
+
+  public static uint LargestPower2(uint n)
+  {
+    return n == 0 ? 0 : 1u << BitOperations.Log2(n);
+  }
+}
