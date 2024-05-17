d418af64cde5dbde7fbd11fbc30dd4c234b2fac7
coffeek <ya.kofeek@yandex.ru>
2024-05-17 23:55:44 +0400

    Fluent assertions and test cases in SparseTable tests

--- Solver.Tests/DataStructures/SparseTableTests.cs
+++ Solver.Tests/DataStructures/SparseTableTests.cs
@@ -5,56 +5,41 @@ namespace Solver.Tests.DataStructures;
 [TestFixture]
 public class SparseTableTests
 {
-  [Test]
-  public void GetMinTest()
+  [TestCase(new[] { 1, 2, 3, 4, 5 })]
+  [TestCase(new[] { -1, 5, 10, 0, 23, 24, 8, 9 })]
+  [TestCase(new[] { 1324 })]
+  [TestCase(new[] { 10, 1 })]
+  public void GetMinTest(int[] data)
   {
-    var data = new[]
+    var t = new SparseTable(data);
+    for (int i = 0; i < data.Length; i++)
     {
-      new[] { 1, 2, 3, 4, 5 }, new[] { -1, 5, 10, 0, 23, 24, 8, 9 }, new[] { 1324 }, new[] { 10, 1 }
-    };
-    foreach (var d in data)
-    {
-      var t = new SparseTable(d);
-      for (int i = 0; i < d.Length; i++)
+      for (int j = i; j < data.Length; j++)
       {
-        for (int j = i; j < d.Length; j++)
-        {
-          var expected = d.Skip(i).Take(j - i + 1).Min();
-          Assert.That(t.GetMin(i, j), Is.EqualTo(expected));
-          Assert.That(t.GetMinFast(i, j), Is.EqualTo(expected));
-        }
+        var expected = data.Skip(i).Take(j - i + 1).Min();
+        t.GetMin(i, j).Should().Be(expected);
+        t.GetMinFast(i, j).Should().Be(expected);
       }
     }
   }
 
-  [Test]
-  public void GetRightIndexLessThanTest1()
+  [TestCase(new[] { 1, 2, 3, 4, 5 }, 1, 0, 4, -1)]
+  [TestCase(new[] { 1, 2, 3, 4, 5 }, 2, 0, 4, 0)]
+  [TestCase(new[] { 1, 2, 3, 4, 5 }, 3, 0, 4, 1)]
+  [TestCase(new[] { 1, 2, 3, 4, 5 }, 4, 0, 4, 2)]
+  [TestCase(new[] { 1, 2, 3, 4, 5 }, 5, 0, 4, 3)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 2, 1, 3, -1)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 3, 1, 3, 2)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 5, 1, 3, 2)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 8, 1, 3, 3)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 8, 4, 4, -1)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 9, 4, 4, 4)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 4, 0, 8, 8)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 3, 0, 8, 7)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 2, 0, 8, 6)]
+  [TestCase(new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 }, 1, 0, 8, -1)]
+  public void GetRightIndexLessThanTest(int[] data, int val, int l, int r, int expected)
   {
-    var d = new[] { 1, 2, 3, 4, 5 };
-    var t = new SparseTable(d);
-    Assert.That(t.GetRightIndexLessThan(1, 0, 4), Is.EqualTo(-1));
-    Assert.That(t.GetRightIndexLessThan(2, 0, 4), Is.EqualTo(0));
-    Assert.That(t.GetRightIndexLessThan(3, 0, 4), Is.EqualTo(1));
-    Assert.That(t.GetRightIndexLessThan(4, 0, 4), Is.EqualTo(2));
-    Assert.That(t.GetRightIndexLessThan(5, 0, 4), Is.EqualTo(3));
-  }
-
-  [Test]
-  public void GetRightIndexLessThanTest2()
-  {
-    var d = new[] { 4, 4, 2, 7, 8, 9, 1, 2, 3 };
-    var t = new SparseTable(d);
-    Assert.That(t.GetRightIndexLessThan(2, 1, 3), Is.EqualTo(-1));
-    Assert.That(t.GetRightIndexLessThan(3, 1, 3), Is.EqualTo(2));
-    Assert.That(t.GetRightIndexLessThan(5, 1, 3), Is.EqualTo(2));
-    Assert.That(t.GetRightIndexLessThan(8, 1, 3), Is.EqualTo(3));
-      
-    Assert.That(t.GetRightIndexLessThan(8, 4, 4), Is.EqualTo(-1));
-    Assert.That(t.GetRightIndexLessThan(9, 4, 4), Is.EqualTo(4));
-      
-    Assert.That(t.GetRightIndexLessThan(4, 0, 8), Is.EqualTo(8));
-    Assert.That(t.GetRightIndexLessThan(3, 0, 8), Is.EqualTo(7));
-    Assert.That(t.GetRightIndexLessThan(2, 0, 8), Is.EqualTo(6));
-    Assert.That(t.GetRightIndexLessThan(1, 0, 8), Is.EqualTo(-1));
+    new SparseTable(data).GetRightIndexLessThan(val, l, r).Should().Be(expected);
   }
 }
