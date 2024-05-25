5220d471bdb5f53f076582d09f8e3dc72c151100
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-05-25 23:54:32 +0400

    Added Trie structure and tests

---
+++ Solver.Tests/DataStructures/TrieTests.cs
@@ -0,0 +1,20 @@
+﻿using Solver.DataStructures;
+
+namespace Solver.Tests.DataStructures;
+
+[TestFixture]
+public class TrieTests
+{
+  [Test]
+  public void Test()
+  {
+    var t = new Trie(new[] { "apple", "pen", "applepen", "pine", "pineapple" });
+    t.Contains("apple").Should().BeTrue();
+    t.Contains("pen").Should().BeTrue();
+    t.Contains("applepen").Should().BeTrue();
+    t.Contains("pin").Should().BeFalse();
+    t.Contains("pinea").Should().BeFalse();
+    t.Contains("").Should().BeFalse();
+    t.Contains("pineapplepen").Should().BeFalse();
+  }
+}
---
+++ Solver/DataStructures/Trie.cs
@@ -0,0 +1,47 @@
+﻿namespace Solver.DataStructures;
+
+/// <summary>
+/// Trie (/ˈtraɪ/, /ˈtriː/), also called "digital tree" or "prefix tree".
+/// https://en.wikipedia.org/wiki/Trie
+/// </summary>
+public class Trie
+{
+  private class TrieNode
+  {
+    public readonly TrieNode[] Next = new TrieNode[26];
+    public bool EndOfWord;
+  }
+
+  private readonly TrieNode root;
+
+  public void Add(string word) => Add(root, word);
+
+  public bool Contains(string word)
+  {
+    var node = root;
+    foreach (var c in word)
+    {
+      node = node.Next[c - 'a'];
+      if (node is null)
+        return false;
+    }
+    return node.EndOfWord;
+  }
+
+  private static TrieNode Build(IList<string> dictionary)
+  {
+    var root = new TrieNode();
+    foreach (var word in dictionary)
+      Add(root, word);
+    return root;
+  }
+
+  private static void Add(TrieNode root, string value)
+  {
+    foreach (var c in value)
+      root = root.Next[c - 'a'] ??= new TrieNode();
+    root.EndOfWord = true;
+  }
+
+  public Trie(IList<string> words) => root = Build(words);
+}
