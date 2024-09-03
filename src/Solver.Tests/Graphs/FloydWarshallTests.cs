ec9e45aea434281f4f5e0dce71652bf0a12b8895
coffeek <ya.kofeek@yandex.ru>
2024-09-03 20:36:19 +0400

    Tests for Floyd-Warshall

---
+++ src/Solver.Tests/Graphs/FloydWarshallTests.cs
@@ -0,0 +1,55 @@
+using Solver.Graphs;
+
+namespace Solver.Tests.Graphs;
+
+[TestFixture]
+public class FloydWarshallTests
+{
+  [Test]
+  public void FindDistance_ForAdjMatrixGraph()
+  {
+    const int inf = (int)1e9;
+    int[][] distanceMatrix =
+    {
+      new[] { inf, 3, inf, inf }, 
+      new[] { 3, inf, 1, 4 }, 
+      new[] { inf, 1, inf, 1 }, 
+      new[] { inf, 4, 1, inf }
+    };
+    FloydWarshall.FindDistances(4, distanceMatrix);
+
+    distanceMatrix.Should()
+      .BeEquivalentTo(new[]
+        {
+          new[] { 6, 3, 4, 5 }, 
+          new[] { 3, 2, 1, 2 }, 
+          new[] { 4, 1, 2, 1 }, 
+          new[] { 5, 2, 1, 2 }
+        },
+        o => o.WithStrictOrdering());
+  }
+
+  [Test]
+  public void FindDistance_ForListOfEdgesGraph()
+  {
+    var graph = new List<(int to, int w)>[4]
+    {
+      new() { (1, 3), }, 
+      new() { (0, 3), (2, 1), (3, 4) }, 
+      new() { (1, 1), (3, 1) }, 
+      new() { (2, 1), (1, 4) },
+    };
+    
+    var distanceMatrix = FloydWarshall.FindDistances(graph);
+
+    distanceMatrix.Should()
+      .BeEquivalentTo(new[]
+        {
+          new[] { 6, 3, 4, 5 }, 
+          new[] { 3, 2, 1, 2 }, 
+          new[] { 4, 1, 2, 1 }, 
+          new[] { 5, 2, 1, 2 }
+        },
+        o => o.WithStrictOrdering());
+  }
+}
