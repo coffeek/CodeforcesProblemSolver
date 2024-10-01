54d9c67e4ea7f2d0c44c1a04f071f84fc1712b3f
coffeek <ya.kofeek@yandex.ru>
2024-10-01 18:59:37 +0400

    Solver.Geometry.Tests project

--- src/Solver.Geometry.Tests/GeometryTests.cs
+++ src/Solver.Geometry.Tests/GeometryTests.cs
@@ -1,6 +1,6 @@
-﻿using System.Numerics;
+﻿using NUnit.Framework;
 
-namespace Solver.Tests.Geometry;
+namespace Solver.Geometry.Tests;
 
 [TestFixture]
 public class GeometryTests
@@ -11,17 +11,17 @@ public class GeometryTests
   [TestCase(-0.00001f, -1)]
   public void CmpZTest(float value, int expected)
   {
-    Solver.Geometry.Geometry.CmpZ(value).Should().Be(expected);
+    Geometry.CmpZ(value).Should().Be(expected);
   }
 
   [Test]
   public void ReflectTest()
   {
-    Solver.Geometry.Geometry.Reflect(new Vector3(-1, 0, 1), new Vector3(0, 0, 1))
+    Geometry.Reflect(new Vector3(-1, 0, 1), new Vector3(0, 0, 1))
       .Should().Be(new Vector3(-1, 0, -1));
-    Solver.Geometry.Geometry.Reflect(new Vector3(0, -1, 1), new Vector3(0, 0, 1))
+    Geometry.Reflect(new Vector3(0, -1, 1), new Vector3(0, 0, 1))
       .Should().Be(new Vector3(0, -1, -1));
-    Solver.Geometry.Geometry.Reflect(new Vector3(-1, -1, -1), new Vector3(0, 0, 1))
+    Geometry.Reflect(new Vector3(-1, -1, -1), new Vector3(0, 0, 1))
       .Should().Be(new Vector3(-1, -1, 1));
   }
 }
